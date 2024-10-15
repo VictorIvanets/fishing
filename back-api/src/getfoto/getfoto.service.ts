f192538f01410400145bbd39a196e4d1b4b862b7
Виктор <imperia.zt@gmail.com>
2024-10-15 19:43:58 +0300

    resize pic

--- back-api/src/getfoto/getfoto.service.ts
+++ back-api/src/getfoto/getfoto.service.ts
@@ -5,6 +5,7 @@ import { MFile } from 'src/fotoset/mfile.class'
 import { ModelType } from '@typegoose/typegoose/lib/types'
 import { InjectModel } from 'nestjs-typegoose'
 import { GetFotoModel } from './getfoto.model'
+import * as sharp from 'sharp'
 
 @Injectable()
 export class GetfotoService {
@@ -32,8 +33,25 @@ export class GetfotoService {
 		} else return res
 	}
 
+	convertToJpegMin(file: Buffer): Promise<Buffer> {
+		const size = file.byteLength
+		if (size <= 200000) {
+			return sharp(file).jpeg().toBuffer()
+		}
+		if (size > 200000 && size <= 500000) {
+			return sharp(file).jpeg({ quality: 75 }).toBuffer()
+		}
+		if (size > 500000 && size <= 2000000) {
+			return sharp(file).resize(1000).jpeg({ quality: 75 }).toBuffer()
+		}
+		if (size > 2000000) {
+			return sharp(file).resize(1500).jpeg({ quality: 75 }).toBuffer()
+		}
+	}
+
 	async saveFotoBd(files: MFile[], folder: string): Promise<string> {
 		for (const file of files) {
+			const minbuffer = await this.convertToJpegMin(file.buffer)
 			const filename = file.originalname
 			const fotoset = await this.getFotoModel.findOne({ filename }).exec()
 			const check = fotoset?.setid === folder ? true : false
@@ -42,7 +60,7 @@ export class GetfotoService {
 				const res = new this.getFotoModel({
 					setid: folder,
 					filename: file.originalname,
-					imgBuffer: file.buffer,
+					imgBuffer: minbuffer,
 				})
 
 				res.save()
